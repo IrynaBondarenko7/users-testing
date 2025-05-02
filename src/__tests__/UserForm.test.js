a0689d4a773ae29eaf1b8d231cec105b2a6641f1
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-05-02 13:56:12 +0100

    added 2 user form tests

---
+++ src/__tests__/UserForm.test.js
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import UserForm from "../components/UserForm";
+
+test("UserForm shows 2 inputs and a button", () => {
+  render(<UserForm />);
+
+  const inputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
+
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
+});
+
+test("UserForm component calls onUserAdd func when the form is submitted", () => {
+  const mock = jest.fn();
+
+  render(<UserForm onUserAdd={mock} />);
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+
+  user.click(nameInput);
+  user.keyboard("Jane");
+
+  user.click(emailInput);
+  user.keyboard("jane@jane.com");
+
+  const button = screen.getByRole("button");
+
+  user.click(button);
+
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({ name: "Jane", email: "jane@jane.com" });
+});
